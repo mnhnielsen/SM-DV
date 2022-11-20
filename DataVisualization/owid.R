135c05625262263f5b11b0c53fee73a7ad513b2a
Frederik Nielsen <frederik@icsharp.io>
2022-11-20 15:03:54 +0100

    somewhat done with new_vaccinations and new_cases

--- DataVisualization/owid.R
+++ DataVisualization/owid.R
@@ -7,48 +7,71 @@ library(shinythemes)
 library(readxl)
 
 CovidDenmark <- read_excel("CovidDenmark.xlsx")
+CovidDenmark$date <- as.Date(CovidDenmark$date, format = "%Y-%m-%d")
 
 
-
-
-
-ui <- fluidPage(theme = shinytheme("superhero"),
-                
+ui <- fluidPage(
+  theme = shinytheme("superhero"),
+  
   titlePanel("Corona data"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "continent", label = "Continent",
-                  choices = unique(c(CovidDenmark$continent))),
-      selectInput(inputId = "country", label = "Country",
-                  choices = unique(c(CovidDenmark$location)))),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Plots", plotOutput("plots")),
-        tabPanel("Animated", plotOutput("aniamted"))
-
+      selectInput(
+        inputId = "country",
+        label = "Country",
+        choices = unique(c(CovidDenmark$location)),
+        multiple = TRUE,
+        selected = "Denmark"
+      ),
+      selectInput(
+        inputId = "outcome",
+        label = "Parameter",
+        choices = c(
+          "Total Cases" = colnames(CovidDenmark)[5],
+          "New Cases" = colnames(CovidDenmark)[6],
+          "Total Deaths" = colnames(CovidDenmark)[8]
+        )
       )
-    )
+    ),
+    
+    mainPanel(tabsetPanel(
+      tabPanel("Plots", plotOutput("plots")),
+      tabPanel("Map", plotOutput("map"))
+      
+    ))
   )
 )
 
 
-server<-function(input,output){
-  output$plots<-renderPlot({
-    #ggplot(filter(CovidDenmark, continent==input$continent), aes(x=total_cases,y=total_deaths, color=location)) + geom_point()
-    ggplot(filter(CovidDenmark, continent==input$continent), aes(x=date,y=total_vaccinations, color=location)) + geom_point()
+server <- function(input, output) {
+  output$plots <- renderPlot({
+    ggplot(
+      filter(CovidDenmark, location == input$country),
+      aes(
+        x = date,
+        y = !!as.symbol(input$outcome),
+        color = location
+      )
+    ) + geom_line(size = 1) +
+      scale_x_date(date_labels = "%m-%Y") +
+      xlab("Date") +
+      ylab(input$outcome) +
+      theme_classic()
   })
-  
-  output$aniamted<-renderPlot({
-    #ggplot(filter(CovidDenmark, continent==input$continent), aes(x=total_cases,y=total_deaths, color=location)) + geom_point()
-    ggplot(filter(CovidDenmark, continent==input$continent), aes(x=date,y=total_vaccinations, color=location)) + geom_point()
-    
+  output$map <- renderPlot({
+    ggplot(
+      filter(CovidDenmark, location == input$country),
+      aes(x = date, y = new_cases, color = location)) + 
+      geom_bar(aes(x = date, y = new_vaccinations),
+      stat = "identity",
+      fill = "cyan",
+      colour = "green") +
+      geom_line(size = 1) +
+      scale_x_date(date_labels = "%m-%Y") +
+      xlab("Date") +
+      theme_classic()
   })
 }
-
-
-
-
-
-shinyApp(ui,server)
+shinyApp(ui, server)
