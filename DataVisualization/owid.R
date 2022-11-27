9f8379c7576a00df2808a2891112595927015323
mnhnielsen <mnhnielsen@gmail.com>
2022-11-27 16:48:34 +0100

    mathias merge

--- DataVisualization/owid.R
+++ DataVisualization/owid.R
@@ -15,7 +15,9 @@ library(gifski)
 library(purrr)
 
 
+
 CovidDenmark <- read_excel("CovidDenmark.xlsx")
+CovidDenmark$date <- as.Date(CovidDenmark$date, format="%Y-%m-%d")
 
 datadk = filter(CovidDenmark, location == "Denmark")
 datasw = filter(CovidDenmark, location == "Sweden")
@@ -47,15 +49,16 @@ ui <- fluidPage(theme = shinytheme("superhero"),
   titlePanel("Corona data"),
 
 # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "continent", label = "Continent",
-                  choices = unique(c(CovidDenmark$continent))),
-      selectInput(inputId = "country", label = "Country",
-                  choices = unique(c(CovidDenmark$location)))),
+sidebarLayout(
+  sidebarPanel(
+    selectInput(inputId = "country", label = "Country",
+                choices = unique(c(CovidDenmark$location)), multiple = TRUE, selected = "Denmark"),
+    selectInput(inputId = "outcome", label = "Parameter",
+                choices = c("Total Cases"=colnames(CovidDenmark)[5], "New Cases"=colnames(CovidDenmark)[6], "Total Deaths"=colnames(CovidDenmark)[8]))),
     mainPanel(
       tabsetPanel(
         tabPanel("Plots", plotOutput("plots")),
+        tabPanel("New deaths vs Vaccination in Sweden", plotOutput("anim")),
         tabPanel("Animated", imageOutput(outputId = "aniamted", width = "100%")),
         tabPanel("Lasse-Barchart", plotOutput(outputId = "barplot"), radioButtons(inputId = "var", label = "Choose desired Variable!",
                                                                      choices = c("Total Deaths" = "total_deaths", "Total Vaccinations" = "total_vaccinations", "Total Cases" = "total_cases"), selected = "total_deaths"))
@@ -118,6 +121,22 @@ server <- function(input, output) {
       geom_bar(stat = 'identity') +
       ylab(input$var)
 
+  })
+  output$plots<-renderPlot({  
+    ggplot(filter(CovidDenmark, location==input$country), aes(x=date,y=!!as.symbol(input$outcome), color=location)) + 
+      geom_line(size = 1) + 
+      scale_x_date(date_labels = "%m-%Y") +
+      xlab("Date") + 
+      ylab(input$outcome) +
+      theme_classic()
+  })
+  output$anim<-renderPlot({  
+    ggplot(filter(CovidDenmark, location=="Sweden"), aes(x=date,y=new_vaccinations_smoothed_per_million, color=location)) + 
+      geom_line(size = 1) + geom_line(aes(x=date, y=new_deaths)) +
+      scale_x_date(date_labels = "%m-%Y") +
+      xlab("Date") + 
+      ylab("New Vaccinations and New Deaths") +
+      theme_classic()
   })
 
 
