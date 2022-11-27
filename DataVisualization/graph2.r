7b75c6536fd0b297a2a1088272ed11dc5af96264
MathiasHansen95 <maha520@student.sdu.dk>
2022-11-27 15:42:13 +0100

    both graphs done (kunne godt være pænere)

---
+++ DataVisualization/graph2.r
@@ -0,0 +1,50 @@
+library(shiny)
+library(readxl)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(shinythemes)
+library(readxl)
+library(ggborderline)
+
+CovidDenmark <- read_excel("CovidDenmark.xlsx")
+covidWorld <- read_excel("owid-covid-data.xlsx")
+
+
+
+
+
+ui <- fluidPage(theme = shinytheme("superhero"),
+                
+                titlePanel("Corona data"),
+                
+                # Sidebar with a slider input for number of bins 
+                sidebarLayout(
+                  sidebarPanel(
+                    selectInput(inputId = "country", label = "Country",
+                                choices = unique(c(CovidDenmark$location)), multiple = TRUE, selected = "Denmark")),
+                  mainPanel(
+                    tabsetPanel(
+                      tabPanel("Plots", plotOutput("plots")),
+                      tabPanel("Animated", plotOutput("aniamted"))
+                      
+                    )
+                  )
+                )
+)
+
+datebreaks <- seq(as.Date("2020-02-27"), as.Date("2022-10-03"), by = "1 month")
+
+
+server<-function(input,output){
+  output$plots<-renderPlot({
+    
+    ggplot(filter(CovidDenmark, location==input$country), aes( x = reproduction_rate, y=stringency_index, color=location)) +
+      geom_histogram(stat = "identity", size = 1)
+  })
+  
+}
+
+    
+shinyApp(ui,server)
+    
