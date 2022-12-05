673434c9ec82a4277683ee88a5fe49ceea11ba54
mnhnielsen <mnhnielsen@gmail.com>
2022-12-05 10:53:31 +0100

    clean up & normalization

--- DataVisualization/owid.R
+++ DataVisualization/owid.R
@@ -24,24 +24,24 @@ datadk = filter(CovidDenmark, location == "Denmark")
 datasw = filter(CovidDenmark, location == "Sweden")
 datano = filter(CovidDenmark, location == "Norway")
 
-tddk = max(datadk$total_deaths, na.rm = TRUE)
-tdsw = max(datasw$total_deaths, na.rm = TRUE)
-tdno = max(datano$total_deaths, na.rm = TRUE)
+tddk = max(datadk$total_deaths_per_million, na.rm = TRUE)
+tdsw = max(datasw$total_deaths_per_million, na.rm = TRUE)
+tdno = max(datano$total_deaths_per_million, na.rm = TRUE)
 
-tcdk = max(datadk$total_cases, na.rm = TRUE)
-tcsw = max(datasw$total_cases, na.rm = TRUE)
-tcno = max(datano$total_cases, na.rm = TRUE)
+tcdk = max(datadk$total_cases_per_million, na.rm = TRUE)
+tcsw = max(datasw$total_cases_per_million, na.rm = TRUE)
+tcno = max(datano$total_cases_per_million, na.rm = TRUE)
 
-tvdk = max(datadk$total_vaccinations, na.rm = TRUE)
-tvsw = max(datasw$total_vaccinations, na.rm = TRUE)
-tvno = max(datano$total_vaccinations, na.rm = TRUE)
+tvdk = max(datadk$total_vaccinations_per_hundred, na.rm = TRUE)
+tvsw = max(datasw$total_vaccinations_per_hundred, na.rm = TRUE)
+tvno = max(datano$total_vaccinations_per_hundred, na.rm = TRUE)
 
-total_vaccinations = c(tvdk, tvsw, tvno)
-total_deaths = c(tddk, tdsw, tdno)
-total_cases = c(tcdk, tcsw, tcno)
+total_vaccinations_per_hundred = c(tvdk, tvsw, tvno)
+total_deaths_per_million = c(tddk, tdsw, tdno)
+total_cases_per_million = c(tcdk, tcsw, tcno)
 locations = unique(c(CovidDenmark$location))
 
-barFrame = data.frame(locations, total_vaccinations, total_deaths, total_cases)
+barFrame = data.frame(locations, total_vaccinations_per_hundred, total_deaths_per_million, total_cases_per_million)
 
 
 
@@ -49,13 +49,13 @@ ui <- fluidPage(theme = shinytheme("superhero"),
 
   titlePanel("Corona data"),
 
-# Sidebar with a slider input for number of bins 
+
 sidebarLayout(
   sidebarPanel(
     selectInput(inputId = "country", label = "Country",
                 choices = unique(c(CovidDenmark$location)), multiple = TRUE, selected = "Denmark"),
     selectInput(inputId = "outcome", label = "Parameter",
-                choices = c("Total Cases"=colnames(CovidDenmark)[5], "New Cases"=colnames(CovidDenmark)[6], "Total Deaths"=colnames(CovidDenmark)[8]))),
+                choices = c("Total Cases per million"=colnames(CovidDenmark)[11], "New Cases per million"=colnames(CovidDenmark)[12], "Total Deaths per million"=colnames(CovidDenmark)[14]))),
     mainPanel(
       tabsetPanel(
         tabPanel("Plots", plotOutput("plots")),
@@ -66,7 +66,7 @@ sidebarLayout(
         tabPanel("New deaths vs Vaccination in Sweden", plotOutput("anim")),
         tabPanel("Animated", imageOutput(outputId = "aniamted", width = "100%")),
         tabPanel("Barchart", plotOutput(outputId = "barplot"), radioButtons(inputId = "var", label = "Choose desired Variable!",
-                                                                     choices = c("Total Deaths" = "total_deaths", "Total Vaccinations" = "total_vaccinations", "Total Cases" = "total_cases"), selected = "total_deaths"))
+                                                                     choices = c("Total Deaths per million" = "total_deaths_per_million", "Total Vaccinations per hundred" = "total_vaccinations_per_hundred", "Total Cases per million" = "total_cases_per_million"), selected = "total_deaths_per_million"))
 
       )
     )
@@ -86,10 +86,10 @@ server <- function(input, output) {
 
 
     graph =
-      ggplot(CovidDenmark, aes(x = as.Date(date), y = total_deaths, colour = location)) + geom_line(size = 2, alpha = 0.75) +
+      ggplot(CovidDenmark, aes(x = as.Date(date), y = total_deaths_per_million, colour = location)) + geom_line(size = 2, alpha = 0.75) +
       theme_solarized_2() +
-      labs(title = "Deaths vs Total cases in Scandinavia",
-           y = "Total Deaths",
+      labs(title = "Deaths in Scandinavia over time",
+           y = "Total Deaths per million",
            x = "Date") +
       theme(text = element_text(family = "DM Sans Medium", colour = "#000000"),
             title = element_text(colour = "#000000"),
@@ -99,7 +99,7 @@ server <- function(input, output) {
            )
 
     #graph.animation=graph+transition_reveal(as.Date(date))+
-    #view_follow(fixed_y = TRUE)
+    #(fixed_y = TRUE)
 
 
 
@@ -107,11 +107,10 @@ server <- function(input, output) {
 
     list(src = "outfile.gif",
          contentType = 'image/gif',
-         width = 800,
-         height = 600
-
-    )
-  }) #, deleteFile = FALSE)
+         width=800,
+         height=600
+         
+    )})#, deleteFile = FALSE)
 
 
 
@@ -124,7 +123,7 @@ server <- function(input, output) {
 
     data = data_r()
 
-    ggplot(data, aes(x = locations, y = y, fill=locations)) +
+    ggplot(data, aes(x = locations, y = y)) +
       geom_bar(stat = 'identity') +
       ylab(input$var)
 
@@ -139,7 +138,8 @@ server <- function(input, output) {
   })
   output$anim<-renderPlot({  
     ggplot(filter(CovidDenmark, location=="Sweden"), aes(x=date,y=new_vaccinations_smoothed_per_million, color=location)) + 
-      geom_line(size = 1) + geom_line(aes(x=date, y=new_deaths)) +
+      geom_line(size = 1) + geom_line(aes(x=date, y=new_deaths_per_million*25), color="blue") +
+      scale_y_continuous(sec.axis = sec_axis(~./25, "New Deaths per million")) +
       scale_x_date(date_labels = "%m-%Y") +
       xlab("Date") + 
       ylab("New Vaccinations and New Deaths") +
@@ -148,46 +148,50 @@ server <- function(input, output) {
 
   output$histo<-renderPlot({
     
-    ggplot(filter(CovidDenmark, location==input$country), aes(x= as.Date(date), y=new_cases, color=location)) + 
+    ggplot(filter(CovidDenmark, location==input$country), aes(x=date, y=new_cases_per_million, color=location)) + 
       geom_histogram(stat = "identity", size = 1) + 
-      geom_borderline(aes(y=icu_patients * 100), size=1, bordercolour = "black") +
-      scale_y_continuous(sec.axis = sec_axis(~./100, "icu patients")) +
+      geom_line(aes(y=icu_patients_per_million * 100), size=1, bordercolour = "black", color="blue") +
+      scale_y_continuous(sec.axis = sec_axis(~./100, "Icu Patients per million")) +
       xlab("Date") + 
       theme_classic() +
-      scale_x_date(breaks = datebreaks) +
+      scale_x_date(date_labels = "%m-%Y") +
       theme(axis.text.x = element_text(angle = 30, hjust = 1))
   })
   output$icu<-renderPlot({
     
     ggplot(filter(CovidDenmark, location==input$country), aes( x = reproduction_rate, y=stringency_index / 13)) +
-      geom_histogram(bins= 15, stat="identity",  size = 1)
+      geom_histogram(bins= 15, stat="identity",  size = 1) + 
+      xlab("Reproduction Rate") + 
+      ylab("Stringency Indexs")
   })
   output$newVac <- renderPlot({
     ggplot(
       filter(CovidDenmark, location == input$country),
-      aes(x = date, y = new_cases, color = location)) + 
-      geom_bar(aes(x = date, y = new_vaccinations),
+      aes(x = date, y = new_cases_per_million, color = location)) + 
+      geom_bar(aes(x = date, y = new_vaccinations_smoothed_per_million),
                stat = "identity",
                fill = "pink",
                colour = "pink") +
       geom_line(size = 1) +
       scale_x_date(date_labels = "%m-%Y") +
       xlab("Date") +
+      ylab("New Cases pr million") +
       theme_linedraw()
   })
   
   output$newTest <- renderPlot({
     ggplot(
       filter(CovidDenmark, location == input$country),
-      aes(x = date, y = new_tests_smoothed_per_thousand, color = location)) + 
+      aes(x = date, y = new_tests_smoothed_per_thousand/15, color = location)) + 
       geom_bar(aes(x = date, y = positive_rate),
                stat = "identity",
                fill = "cyan",
                colour = "purple") +
-      scale_y_continuous(sec.axis = sec_axis( ~ . * 0.0001, name = "Positive Rate")) +
+      scale_y_continuous(sec.axis = sec_axis( ~ ./15, name = "Positive Rate")) +
       geom_line(size = 1) +
       scale_x_date(date_labels = "%m-%Y") +
       xlab("Date") +
+      ylab("New tests per thousand") +
       theme_linedraw()  
   })
 }
